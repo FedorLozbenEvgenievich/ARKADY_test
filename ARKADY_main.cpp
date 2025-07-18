4edda8e45d80848877790184fa664a45a43ddcf1
FedorLozbenEvgenievich <f.lozben@g.nsu.ru>
2025-07-18 18:40:25 +0700

    Add files via upload
    made fever ugly and more friendly interface + added statistics check,
    Seems good enough to say it is done.

--- ARKADY_main.cpp
+++ ARKADY_main.cpp
@@ -19,15 +19,15 @@ __int64 fac(int a) {
 }
 
 int main() {
-	const int Nmax=10000;
-	bool there=true,start=false;
+	const int Nmax=50000;
+	bool there=true,start=false,iterate=false;
 	int position=1,position_=1,cond=1;
 	char ch;
-	int N=1000,T=100000;
+	int N=1000,T=100000,T_skip=1;
 	float v=0.2,r=1.5,p_kill=0.001,p_heal=0,p_sick=0.1;
 	float Ob_x[Nmax],Ob_y[Nmax],Ob_aim[Nmax],Ob_dx[Nmax],Ob_dy[Nmax];
-	int Ob_t[Nmax];
-	char Ob_cond[Nmax];
+	unsigned int Ob_t[Nmax];
+	unsigned char Ob_cond[Nmax];
 	float t_distr[100];
 	
 	int Ob_mx[Nmax],Ob_my[Nmax];
@@ -59,7 +59,6 @@ int main() {
 			if ((t_distr_inv[j]>=i/100.0)&&(t_distr_inv[j-1]<=i/100.0)) {t_distr[i]=j;}
 		}
 	}
-	getch();
 	
 	while (there==true) {
 		system("cls");
@@ -68,31 +67,37 @@ int main() {
 		if (position==1) {printf("|->");} else {printf("|  ");}
 		printf("      START|");
 		if ((cond>=2)&&(position_==1)) {printf("|->");} else {printf("|  ");}
-		if (cond>=2) {printf(" 1<=[N=%5d]<=%d   cahnge->|",N,Nmax);}
+		if (cond>=2) {printf(" 1<=[N=%5d]<=%5d   cahnge->|",N,Nmax);}
 		printf("\n");
 		
 		if (position==2) {printf("|->");} else {printf("|  ");}
 		printf(" PARAMETRES|");
 		if ((cond>=2)&&(position_==2)) {printf("|->");} else {printf("|  ");}
-		if (cond>=2) {printf(" dying   p=%5f  cahnge->|",p_kill);}
+		if (cond>=2) {printf(" dying   p=%5f    cahnge->|",p_kill);}
 		printf("\n");
 		
 		if (position==3) {printf("|->");} else {printf("|  ");}
 		printf("        OUT|");
 		if ((cond>=2)&&(position_==3)) {printf("|->");} else {printf("|  ");}
-		if (cond>=2) {printf(" healing p=%5f  cahnge->|",p_heal);}
+		if (cond>=2) {printf(" healing p=%5f    cahnge->|",p_heal);}
 		printf("\n");
 		
 		printf("|  ");
 		printf("           |");
 		if ((cond>=2)&&(position_==4)) {printf("|->");} else {printf("|  ");}
-		if (cond>=2) {printf(" 1<=[T=%5d]     cahnge->|",T);}
+		if (cond>=2) {printf(" 1<=[T=%5d]         cahnge->|",T);}
 		printf("\n");
 		
 		printf("|  ");
 		printf("           |");
 		if ((cond>=2)&&(position_==5)) {printf("|->");} else {printf("|  ");}
-		if (cond>=2) {printf(" catching p=%5f cahnge->|",p_sick);}
+		if (cond>=2) {printf(" catching p=%5f   cahnge->|",p_sick);}
+		printf("\n");
+		
+		printf("|  ");
+		printf("           |");
+		if ((cond>=2)&&(position_==6)) {printf("|->");} else {printf("|  ");}
+		if (cond>=2) {printf(" draw each %5d       cahnge->|",T_skip);}
 		printf("\n");
 		
 		ch=getch();
@@ -105,15 +110,22 @@ int main() {
 		if (cond==2) {
 			if (ch=='w') {position_--;}
 			if (ch=='s') {position_++;}
-			if (position_==0) {position_=5;}
-			if (position_==6) {position_=1;}
+			if (position_==0) {position_=6;}
+			if (position_==7) {position_=1;}
 		}
 		if (ch=='a') {
 			if (cond==2) {cond=1;}
+			if ((cond==1)&&(position==1)) {
+				start=true;
+				iterate=true;
+			}
 		}
 		if (ch=='d') {
 			if (cond==1) {
-				if (position==1) {start=true;}
+				if (position==1) {
+					start=true;
+					iterate=false;
+				}
 				if (position==2) {cond=2;}
 				if (position==3) {there=false;}
 			} else {
@@ -148,20 +160,36 @@ int main() {
 						if (p_sick<0) {p_sick=0;}
 						if (p_sick>1) {p_sick=1;}
 					}
+					if (position_==6) {
+						printf("\n\nskip each=");
+						scanf("%d",&T_skip);
+						if (T_skip<1) {T_skip=1;}
+						if (T_skip>T/2) {T_skip=T/2;}
+					}
 				}
 			}
 		}
 		if (start==true) {
+			system("cls");
+			printf("abort - [SPACE]\n");
+		}
+		float a_stat=0,b_stat=0,c_stat=0,d_stat=0;
+		int n_stat=0,stat_D[100];
+		for (int k=0;k<100;k++) {stat_D[k]=0;}
+		ch=0;
+		while ((start==true)||((iterate==true)&&(ch!=' '))) {
 			start=false;
-			
+			if (iterate==true) {
+				printf("__");
+			}
 			int xx,yy;
 			
 			for (int i=0;i<N;i++) {
-				float ang=(rand()%10000)*2*pi/10000.0;
+				float ang=(rand()%1000)*2*pi/10000.0;
 				Ob_dx[i]=v*cos(ang);
 				Ob_dy[i]=v*sin(ang);
-				Ob_x[i]=(rand()%10000)/10000.0*80;
-				Ob_y[i]=(rand()%10000)/10000.0*100;
+				Ob_x[i]=(rand()%10001)/10000.0*80;
+				Ob_y[i]=(rand()%10001)/10000.0*100;
 				Ob_cond[i]=0;
 				
 				xx=Ob_x[i]/1.5;
@@ -179,9 +207,11 @@ int main() {
 			}
 			
 			Ob_cond[0]=1;
-			Ob_t[0]=t_distr[(int)((rand()%1000)/1000.0 * 100)]*24;
+			Ob_t[0]=t_distr[(int)((rand()%1001)/1000.0 * 100)]*24;
 			
-			for (int t=0;t<T;t++) {
+			ch=0;
+			for (int t=0;(t<T)&&(ch!=' ');t++) {
+				if (kbhit()) {ch=getch();}
 				for (int i=0;i<N;i++) {
 					
 					int xx,yy;
@@ -236,7 +266,7 @@ int main() {
 										int j=mas[xxx][yyy][jj];
 										if ((Ob_cond[j]==1)||(Ob_cond[j]==2)) {
 											if (sq(Ob_x[i]-Ob_x[j])+sq(Ob_y[i]-Ob_y[j])<r*r) {
-												if ((rand()%1000)/1000.0<p_sick) {Ob_cond[i]=1;}
+												if ((rand()%1000+1)/1000.0<p_sick) {Ob_cond[i]=1;}
 											}
 										}
 									}
@@ -244,11 +274,11 @@ int main() {
 							}
 						}
 						if (Ob_cond[i]==1) {
-							Ob_t[i]=t_distr[(int)((rand()%1000)/1000.0 * 100)]*24;
+							Ob_t[i]=t_distr[(int)((rand()%1001)/1000.0 * 100)]*24;
 						}
 					} else {
 						if (Ob_cond[i]!=3) {
-							if ((rand()%1000)/1000.0<p_heal) {
+							if ((rand()%1000+1)/1000.0<p_heal) {
 								Ob_cond[i]=0;
 								if (Ob_x[i]>88) {Ob_x[i]=88;}
 								
@@ -288,7 +318,8 @@ int main() {
 									Ob_t[i]--;
 								}
 							}
-							if ((rand()%1000)/1000.0<p_kill) {
+							
+							if ((Ob_cond[i]==2)&&((rand()%1000+1)/1000.0<p_kill)) {
 								Ob_cond[i]=3;
 								int xx,yy;
 								xx=Ob_mx[i];
@@ -308,40 +339,78 @@ int main() {
 					}
 				}
 				
-				int a=0,b=0,c=0,d=0;
-				for (int i=0;i<N;i++) {
-					if (Ob_cond[i]==0) {a++;}
-					if (Ob_cond[i]==1) {b++;}
-					if (Ob_cond[i]==2) {c++;}
-					if (Ob_cond[i]==3) {d++;}
-				}
-				//printf("%d:%d %d %d %d\n",t,a,b,c,d);
-				float aa=a*20.0/N;
-				float bb=b*20.0/N;
-				float cc=c*20.0/N;
-				float dd=d*20.0/N;
-				a=(int)(aa);
-				b=(int)(bb);
-				c=(int)(cc);
-				d=(int)(dd);
-				if (a+b+c+d<20) {
-					while (a+b+c+d<20) {
-						if ((aa-a>bb-b)&&(aa-a>cc-c)&&(aa-a>dd-d)) {a++;} else {
-							if ((bb-b>aa-a)&&(bb-b>cc-c)&&(bb-b>dd-d)) {b++;} else {
-								if ((cc-c>aa-a)&&(cc-c>bb-b)&&(cc-c>dd-d)) {c++;} else {
-									d++;
+				if (((t%T_skip==0)&&(iterate==false))||((t==T-1)&&(iterate==true))) {
+					int a=0,b=0,c=0,d=0;
+					for (int i=0;i<N;i++) {
+						if (Ob_cond[i]==0) {a++;}
+						if (Ob_cond[i]==1) {b++;}
+						if (Ob_cond[i]==2) {c++;}
+						if (Ob_cond[i]==3) {d++;}
+					}
+					if (iterate==true) {
+						printf("%d %d %d %d\n",a,b,c,d);
+						n_stat++;
+						a_stat+=a;
+						b_stat+=b;
+						c_stat+=c;
+						d_stat+=d;
+						stat_D[d*100/N]+=1;
+					} else {
+						float aa=a*20.0/N;
+						float bb=b*20.0/N;
+						float cc=c*20.0/N;
+						float dd=d*20.0/N;
+						a=(int)(aa);
+						b=(int)(bb);
+						c=(int)(cc);
+						d=(int)(dd);
+						if (a+b+c+d<20) {
+							while (a+b+c+d<20) {
+								if ((aa-a>=bb-b)&&(aa-a>=cc-c)&&(aa-a>=dd-d)) {a++;} else {
+									if ((bb-b>=aa-a)&&(bb-b>=cc-c)&&(bb-b>=dd-d)) {b++;} else {
+										if ((cc-c>=aa-a)&&(cc-c>=bb-b)&&(cc-c>=dd-d)) {c++;} else {
+											d++;
+										}
+									}
 								}
 							}
 						}
+						for (int i=0;i<a;i++) {printf(":) ");}
+						for (int i=0;i<b;i++) {printf(":| ");}
+						for (int i=0;i<c;i++) {printf(":( ");}
+						for (int i=0;i<d;i++) {printf(":X ");}
+						printf("\n");
 					}
 				}
-				for (int i=0;i<a;i++) {printf(":) ");}
-				for (int i=0;i<b;i++) {printf(":| ");}
-				for (int i=0;i<c;i++) {printf(":( ");}
-				for (int i=0;i<d;i++) {printf(":X ");}
-				printf("\n");
 			}
-			getch();
+			if (iterate==false) {
+				printf("press [SPACE] to return back");
+				while (ch!=' ') {ch=getch();}
+			}
+		}
+		ch=0;
+		if (iterate==true) {
+			iterate=false;
+			if (n_stat>0) {
+				printf("\n\n\n");
+				printf("mean:\n");
+				printf(":) - %.5f%\n",a_stat*100.0/n_stat/N);
+				printf(":| - %.5f%\n",b_stat*100.0/n_stat/N);
+				printf(":( - %.5f%\n",c_stat*100.0/n_stat/N);
+				printf(":X - %.5f%\n",d_stat*100.0/n_stat/N);
+				printf("\n death distribution:\n");
+				for (int i=0;i<20;i++) {
+					for (int j=0;j<20;j++) {
+						if (stat_D[5*j]+stat_D[5*j+1]+stat_D[5*j+2]+stat_D[5*j+3]+stat_D[5*j+4]>5*(20-i)*n_stat/100) {printf("## ");} else {printf(".. ");}
+					}
+					printf("\n");
+				}
+				for (int i=0;i<20;i++) {printf("%2d ",i*5);}
+			} else {
+				printf("could not collect data \\_(*.*)_/\n");
+			}
+			printf("press [SPACE] to return back");
+			while (ch!=' ') {ch=getch();}
 		}
 	}
 	printf("Thanx for using");
